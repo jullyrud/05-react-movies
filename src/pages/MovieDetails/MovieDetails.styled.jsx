de78a6c8b7c18cfada0c4456457c9fde8cb1fb8b
jullyrud <94559083+jullyrud@users.noreply.github.com>
2023-03-18 13:08:02 +0100

    Update MovieDetails.styled.jsx

--- src/pages/MovieDetails/MovieDetails.styled.jsx
+++ src/pages/MovieDetails/MovieDetails.styled.jsx
@@ -2,19 +2,21 @@
 import styled from '@emotion/styled'
 
 export const FimWrap = styled.div`
-/* margin-left: auto;
-margin-right: auto;
-display: block;
-padding: 20px 30px; */
+
 `
 
 export const AditInfo = styled.ul`
 display: flex;
 flex-direction: column;
+
 `
 
 export const MovieContainer = styled.div`
+@media screen and (min-width: 480px) {
 display: flex;
+  
+}
+
 `
 
 export const MovieImgBox = styled.div`
